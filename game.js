c92dc33d0faaec3edbb7014561078bcb429bb46e
Jorge Alvarado <111322460+JorgeSoftwareDev@users.noreply.github.com>
2024-10-08 10:24:05 -0700

    Scoring changes

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
-// Variables for the game canvas
+// ## VARIABLES ##
 let foodX, foodY;
 let speed = 100;
 let totalScore = 0;
-let count = 0.5;
+let level = 1;
+let levelScore = 500 * level;
 let bonus = 300;
-let cBonus = 300;
+let cBonus = bonus * level;
 let scoreBoard = document.getElementById('scoreNum');
+let levelBoard = document.getElementById('levelNum');
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+// ## GAME CANVAS ##
 // Create the game canvas
 ctx.fillStyle = '#8550eb';
 ctx.strokeStyle = '#FFFFFF';
@@ -17,6 +20,12 @@ ctx.strokeRect(0, 0, canvas.width, canvas.height);
 canvas.style.border = '5px solid #000';
 canvas.style.backgroundColor = '#8550eb';
 
+// Clear canvas function
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// ## THE SNAKE ##
 // Array for the snake body
 let snake = [
 	{ x: 150, y: 150 },
@@ -28,8 +37,8 @@ let snake = [
 
 // Functions to draw snake parts
 function drawSnakePart(snakePart) {
-	ctx.fillStyle = 'lightgreen';
-	ctx.strokeStyle = 'darkgreen';
+	ctx.fillStyle = '#90EE90';
+	ctx.strokeStyle = '#06402B';
 	ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
 	ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
@@ -39,13 +48,7 @@ function drawSnake() {
 	snake.forEach(drawSnakePart);
 }
 
-// Clear canvas function
-function clearCanvas() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
 // moving the Snake
-
 // Variables to move the snake
 let dx = 10;
 let dy = 0;
@@ -57,6 +60,7 @@ function advanceSnake() {
 	snake.pop();
 }
 
+// ## CONTROLS ##
 // creating the controls to move the snake
 document.addEventListener('keydown', changeDirection);
 
@@ -90,8 +94,8 @@ function changeDirection(event) {
 
 // creating the food's random position
 function createFood() {
-	foodX = Math.round((Math.random() * (canvas.width - 30)) / 10) * 10;
-	foodY = Math.round((Math.random() * (canvas.height - 30)) / 10) * 10;
+	foodX = Math.round((Math.random() * (canvas.width - 20)) / 10) * 10;
+	foodY = Math.round((Math.random() * (canvas.height - 20)) / 10) * 10;
 }
 
 // Function to draw the actual food on the Canvas
@@ -115,21 +119,53 @@ function advanceSnake() {
 	}
 }
 function updateBonus() {
-	while (cBonus > 0) {
+	if (cBonus > 0) {
 		cBonus--;
 	}
-	console.log('Total score: ' + cBonus);
 }
+
 function update() {
-	cBonus = bonus;
-	speed -= 10;
-	count += 0.5;
-	totalScore = totalScore + 100 * count + cBonus;
-	console.log(speed);
+	rapid();
+	console.log('cBonus: ' + cBonus);
+	console.log('score: ' + totalScore);
+	console.log('speed:' + speed);
+	totalScore += levelScore;
+	totalScore += cBonus;
+	level += 1;
+	cBonus = bonus * level;
+	levelScore = 500 * level;
 }
 
 function scoring() {
-	scoreBoard.textContent = totalScore;
+	scoreBoard.textContent = totalScore.toLocaleString('en-US');
+	levelBoard.textContent = level;
+}
+
+function rapid() {
+	switch (true) {
+		case speed > 80:
+			speed -= 10;
+			break;
+		case speed > 50:
+			speed -= 5;
+			break;
+		case speed > 25:
+			speed -= 1;
+			break;
+		case speed > 3:
+			speed -= 0.5;
+			break;
+		default:
+			speed = 3;
+	}
+}
+
+function didGameEnd() {
+	for (let i = 4; i < snake.length; i++) {
+		const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+		if (didCollide) return true;
+	}
+	const hitLeftWall = snake[0].x < 0;
 }
 
 // Main game function
@@ -138,13 +174,13 @@ function main() {
 		clearCanvas();
 		drawSnake();
 		drawFood();
+		scoring();
 		advanceSnake();
 		updateBonus();
-		scoring();
 		main();
-		console.log(cBonus);
 	}, speed); // Calls the main function every x milliseconds
 }
-main();
+
 createFood();
 scoring();
+main();
